21982c38f03e75431ab704f5fb260466b8691f2b
minalDev-git <mishalminal21@gmail.com>
2026-03-23 15:31:42 +0500

    fixing the deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import Cube from "./components/cube/Cube.jsx";
 import CameraViews from "./components/cube/CameraViews.jsx";
 import OpeningCube from "./components/cube/OpeningCube.jsx";
 import GravityCube from "./components/cube/GravityCube.jsx";
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",
