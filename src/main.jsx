8d31e3023b2293760cfcd39f1ab2f6f582880171
minalDev-git <mishalminal21@gmail.com>
2026-03-23 16:27:53 +0500

    fixing the router

--- src/main.jsx
+++ src/main.jsx
@@ -6,35 +6,30 @@ import Cube from "./components/cube/Cube.jsx";
 import CameraViews from "./components/cube/CameraViews.jsx";
 import OpeningCube from "./components/cube/OpeningCube.jsx";
 import GravityCube from "./components/cube/GravityCube.jsx";
-const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          path: "/",
-          element: <Cube />,
-        },
-        {
-          path: "/camera-views",
-          element: <CameraViews />,
-        },
-        {
-          path: "/eid-wish",
-          element: <OpeningCube />,
-        },
-        {
-          path: "/gravity-cube",
-          element: <GravityCube />,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/HCI_threejs/",
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Cube />,
+      },
+      {
+        path: "/camera-views",
+        element: <CameraViews />,
+      },
+      {
+        path: "/eid-wish",
+        element: <OpeningCube />,
+      },
+      {
+        path: "/gravity-cube",
+        element: <GravityCube />,
+      },
+    ],
   },
-);
+]);
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router}></RouterProvider>,
