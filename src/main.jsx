5f673293fd41611375bf2e540033ddae8769080e
minalDev-git <mishalminal21@gmail.com>
2026-03-23 15:22:05 +0500

    fixing the deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Cube from "./components/cube/Cube.jsx";
